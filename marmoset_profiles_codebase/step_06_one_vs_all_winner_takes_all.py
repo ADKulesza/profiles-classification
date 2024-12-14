26ec2a52e872537ed20cb206d3001dfed1bee7ea
ADKulesza <ad.kulesza122@gmail.com>
2024-12-14 16:10:36 +0100

    global holdout

--- marmoset_profiles_codebase/step_06_one_vs_all_winner_takes_all.py
+++ marmoset_profiles_codebase/step_06_one_vs_all_winner_takes_all.py
@@ -1,4 +1,5 @@
 import argparse
+import glob
 import os
 import re
 
@@ -11,7 +12,6 @@ from read_json import read_json
 
 C_LOGGER_NAME = "mean_metrics"
 
-
 PREDICTIONS_FNAME = "pred_y.npy"
 
 
@@ -21,38 +21,46 @@ def read_data(paths):
     profiles_df = pd.read_csv(paths.profiles_csv)
     logger.info("%s", paths.profiles_csv)
 
-    model_order = read_json(paths.models_order)
-    logger.info("%s", paths.models_order)
-
-    for model_set in model_order[paths.holdout_set]:
+    set_id = paths.holdout_set[-1]
+    eval_paths = glob.glob(paths.evaluation_dir + "/[0-9]*/" + paths.holdout_set + f"/*_set_{set_id}[0-9]" + "/pred_y.npy")
 
 
-        _RE_DIR = re.compile(f"{paths.evaluation_dir}"
-                             + r"[\\/][0-9]*[\\/]"
-                             + f"{paths.holdout_set}"
-                             + r"[\\/].*"
-                             + f"{model_set}"
-                             + r"$"
-                             )
-        for root, dirs, files in os.walk(paths.evaluation_dir):
-            if not _RE_DIR.match(root):
-                continue
+    for _idx, y_pred_path in enumerate(eval_paths):
+        _pred = np.load(y_pred_path)
+        logger.info("DUPA %s", _pred[2])
 
-            _path = os.path.join(root, PREDICTIONS_FNAME)
-            logger.info("PATH:  %s", _path)
-
-            pred_y = np.load(_path)
-
-            argmax_y = np.argmax(pred_y, axis=1)
-            max_y = np.max(pred_y, axis=1)
-            max_y[np.where(argmax_y == 0)[0]] = -max_y[np.where(argmax_y == 0)[0]]
-
-            label_id = re.search(r"[\\/][0-9]*[\\/]", root)[0]
-            logger.info("LABEL ID %s", label_id)
-            profiles_df[f"pred_confidence_{label_id}"] = max_y
-
-
-    logger.info("Loading data... Done!")
+    # model_order = read_json(paths.models_order)
+    # logger.info("%s", paths.models_order)
+    #
+    # for model_set in model_order[paths.holdout_set]:
+    #
+    #
+    #     _RE_DIR = re.compile(f"{paths.evaluation_dir}"
+    #                          + r"[\\/][0-9]*[\\/]"
+    #                          + f"{paths.holdout_set}"
+    #                          + r"[\\/].*"
+    #                          + f"{model_set}"
+    #                          + r"$"
+    #                          )
+    #     for root, dirs, files in os.walk(paths.evaluation_dir):
+    #         if not _RE_DIR.match(root):
+    #             continue
+    #
+    #         _path = os.path.join(root, PREDICTIONS_FNAME)
+    #         logger.info("PATH:  %s", _path)
+    #
+    #         pred_y = np.load(_path)
+    #
+    #         argmax_y = np.argmax(pred_y, axis=1)
+    #         max_y = np.max(pred_y, axis=1)
+    #         max_y[np.where(argmax_y == 0)[0]] = -max_y[np.where(argmax_y == 0)[0]]
+    #
+    #         label_id = re.search(r"[\\/][0-9]*[\\/]", root)[0]
+    #         logger.info("LABEL ID %s", label_id)
+    #         profiles_df[f"pred_confidence_{label_id}"] = max_y
+    #
+    #
+    # logger.info("Loading data... Done!")
 
     return profiles_df
 
@@ -81,20 +89,20 @@ def get_mean_of_area_df(area_df):
 def process(paths):
     profiles_df = read_data(paths)
 
-    output_df = clean_df(output_df)
-    output_df.loc["mean"] = output_df.mean()
-    output_df.loc["std"] = output_df.std()
-
-    output_area_df = get_mean_of_area_df(area_df)
-    output_area_df = output_area_df.loc[
-                     :, ~output_area_df.columns.str.contains("^Unnamed")
-                     ]
-
-    output_df.to_csv(paths.output)
-    logger.info("Results saved to... %s", paths.output)
-
-    output_area_df.to_csv(paths.area_output)
-    logger.info("Results saved to... %s", paths.area_output)
+    # output_df = clean_df(output_df)
+    # output_df.loc["mean"] = output_df.mean()
+    # output_df.loc["std"] = output_df.std()
+    #
+    # output_area_df = get_mean_of_area_df(area_df)
+    # output_area_df = output_area_df.loc[
+    #                  :, ~output_area_df.columns.str.contains("^Unnamed")
+    #                  ]
+    #
+    # output_df.to_csv(paths.output)
+    # logger.info("Results saved to... %s", paths.output)
+    #
+    # output_area_df.to_csv(paths.area_output)
+    # logger.info("Results saved to... %s", paths.area_output)
 
 
 def parse_args():
